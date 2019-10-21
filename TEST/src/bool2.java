702c32e6397b304d940ac17137428c3144077b63
BrickMyself <2457212911@qq.com>
2019-10-21 15:51:51 +0800

    提交代码

---
+++ TEST/src/bool2.java
@@ -0,0 +1,29 @@
+
+public class bool2 {
+
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+/*    boolean b=true;
+    if(b==false)
+    	System.out.println("haha");
+    else
+    	System.out.println("hehe");
+    	
+		       int a=39;  
+		       int b=0x4b;
+		       int c=021;
+		       int d=a+0x12+012;
+		       System.out.println(a);
+		       System.out.println(b);
+		       System.out.println(c);
+		       System.out.println(d);
+		       */
+	       double a=3.0,b=4.0;   // 定义两直角边a 、b
+	       double c=Math.sqrt(a*a+b*b);   //求斜边
+	       System.out.println("Hypotenuse is "+c);  //输出斜边
+
+		}
+
+	}
+
+
