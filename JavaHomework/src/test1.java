bfd0b850255ca065b334ccca34106104325a4e58
BrickMyself <2457212911@qq.com>
2019-11-03 19:48:10 +0800

    提交代码

---
+++ JavaHomework/src/test1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Scanner scanner = new Scanner(System.in);
+		//int num = scanner.nextInt();
+		
+		//int[] nums = new int [1001];
+		//while(num != 0){
+		//	nums[scanner.nextInt()]++;
+		//	num--;
+		//}
+		//for(int i = 0; i <= 1000; ++i){
+		//	if(nums[i] != 0){
+		//		System.out.print(i);
+		//		System.out.print(' ');
+		//	}
+		//}
+		//scanner.close();
+		System.out.print("hello world");
+	}
+}
